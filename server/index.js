df5dcd2fb372e15dea1a5406a45c10a0400783a0
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-24 05:31:58 +0600

    product set  and loade from database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,14 +11,33 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://DB_USER:DB_PASS@cluster0.yyij1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const uri = `mongodb+srv://emaJhonDb1:XbmbfkAExR4njvjB@cluster0.yyij1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('db connections')
-  client.close();
-});
+
+
+
+async function run(){
+    try{
+       await client.connect();
+
+       const productCollections = client.db("emaJhon").collection("product")
+       
+       
+       // get products
+       app.get('/product', async(req,res)=>{
+           const query = {};
+           const cursor = productCollections.find(query);
+           const products = await cursor.toArray()
+           res.send(products)
+       })
+
+    }finally{
+        // client.close();
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
